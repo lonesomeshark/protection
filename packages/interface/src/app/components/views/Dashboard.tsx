e5da36ec0a4c7dc1c3639cb3c7ef8018a9c94b1e
DESKTOP-8SBKEV8\mazda <mazdaswift@gmail.com>
2021-10-10 11:46:40 +0530

    - added gas limit tab

--- packages/interface/src/app/components/views/Dashboard.tsx
+++ packages/interface/src/app/components/views/Dashboard.tsx
@@ -4,6 +4,7 @@ import ethIcon from "../../assets/eth.png";
 import usdcIcon from "../../assets/usdc.png";
 import daiIcon from "../../assets/dai.png";
 import shield from "../../assets/shield.png";
+import { ArrowRightIcon } from "@heroicons/react/outline";
 // import subscribersArtifact from "@lonesomeshark/core/deployed/kovan/Subscribers.json";
 
 import { ethers } from "ethers";
@@ -47,6 +48,7 @@ const icons = {
 }
 function Dashboard() {
     const [isProtected, setIsProtected] = useState(false);
+    const [atIndex, setAtIndex] = useState(0);
     const [customThreshold, setCustomThreshold] = useState(1.01);
     const [userData, setUserData] = useState<UserReserveData[]>();
     const [userPosition, setUserPosition] = useState<UserPosition>()
@@ -196,12 +198,12 @@ const historyView = mockhistory ? mockhistory.map((item, index) => {
 
 const basic = (
     <div className="basic-panel space-y-4">
-        <div className="flex justify-between px-10 pt-10 items-center">
+        <div className="flex justify-between px-10 pt-10 pb-2 items-center">
             <div className="space-y-2">
                 <div className="opacity-50 text-lg">Current Health Factor</div>
                 <div className="text-5xl text-center">1.2</div>
             </div>
-            <div className="pl-10">
+            <div className="lg:pl-20 pl-10">
                 <svg width="52" height="24" viewBox="0 0 52 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path opacity="0.3" d="M51.0607 13.0607C51.6464 12.4749 51.6464 11.5251 51.0607 10.9393L41.5147 1.3934C40.9289 0.807611 39.9792 0.807611 39.3934 1.3934C38.8076 1.97919 38.8076 2.92893 39.3934 3.51472L47.8787 12L39.3934 20.4853C38.8076 21.0711 38.8076 22.0208 39.3934 22.6066C39.9792 23.1924 40.9289 23.1924 41.5147 22.6066L51.0607 13.0607ZM0 13.5H50V10.5H0V13.5Z" fill="black" />
                 </svg>
@@ -216,14 +218,24 @@ const basic = (
             </div>
         </div>
         <div className="flex justify-center pb-8">
-            {!isProtected && <button className="text-white bg-purple px-3 py-2 rounded-md" onClick={() => setIsProtected(!isProtected)}>Protect my assets</button>}
-            {isProtected &&
+            <div className="bg-purple flex rounded-md px-3 py-2 space-x-2">
+                    <div><button className="text-white bg-purple  rounded-md" onClick={() => setAtIndex(1)}> Next</button></div>
+                    <div className="text-white flex items-center"><ArrowRightIcon height="20" /></div>
+                </div>
+            {/* {isProtected &&
                 <div className="flex space-x-4">
                     <button className="text-white bg-purple px-3 py-2 rounded-md" onClick={() => setIsProtected(!isProtected)}>Edit</button>
                     <button className="text-red-type1 border border-red-type1 px-3 py-3 rounded-md">Expose assets to risk</button>
-                </div>}
+                </div>} */}
         </div>
     </div>);
+
+const gasLimitTab = (<div className="pt-6 pl-4 pb-11">
+    <div className="pb-2 opacity-50">Set your gas limit</div>
+    <div className="pb-8 opacity-50 text-5xl max-w-min" contentEditable="true">21000</div>
+    <div>
+        <button className="text-white bg-purple px-3 py-2 rounded-md" onClick={() => setIsProtected(!isProtected)}>Finish & protect my assets</button></div>
+</div>);
 
 const dashboard = (
     <div>
@@ -242,17 +254,17 @@ const dashboard = (
             </div>
             <div className="col-span-2">
                 <Tab.Group
-                    defaultIndex={0}
+                    defaultIndex={atIndex}
                     onChange={index => {
                         console.log(index)
                     }}>
                     <Tab.List className="flex">
-                        <Tab className={({ selected }) => selected ? "border border-gray px-2 py-1 text-purple border-b-0 rounded-t-md" : "border border-gray px-2 py-1 rounded-t-md dark:text-white"}>Basic</Tab>
-                        <Tab className={({ selected }) => selected ? "border border-gray px-2 py-1 text-purple border-b-0 rounded-t-md" : "border border-gray px-2 py-1 rounded-t-md dark:text-white"}>Advanced Options</Tab>
+                        <Tab className={({ selected }) => selected ? "border border-gray px-2 py-1 text-purple border-b-0 rounded-t-md" : "border border-gray px-2 py-1 rounded-t-md dark:text-white"}>1. Set your threshold</Tab>
+                        <Tab className={({ selected }) => selected ? "border border-gray px-2 py-1 text-purple border-b-0 rounded-t-md" : "border border-gray px-2 py-1 rounded-t-md dark:text-white"}>2. Gas Limit</Tab>
                     </Tab.List>
                     <Tab.Panels className="bg-secondary rounded-b-md rounded-tr-md">
                         <Tab.Panel>{basic}</Tab.Panel>
-                        <Tab.Panel><div className="py-24 text-center">Advanced Option</div></Tab.Panel>
+                        <Tab.Panel>{gasLimitTab}</Tab.Panel>
                     </Tab.Panels>
                 </Tab.Group>
             </div>
